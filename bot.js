dc17649976fc7c032234d7b88f5df27b772d3b91
alextanzilli <alextanzilli@gmail.com>
2016-03-30 09:45:02 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegexmfl = /^\/mflpa/;  botRegexDL = /^\/DDL/i; botRegexSalt = /^\/salt/; botRegexRules = /^\/rules/
       botRegexAd=/^\/advance/; botRegexGTA = /^\/gta/; botRegexsdl = /^\/sdl/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
-      botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexporp = /^\/purpose/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegextrades = /^\/trades/;
+      botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexporp = /^\/purpose/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegextrades = /^\/trades/; botRegexFats = /^\/fatty/;
       botRegexSiege = /^\/siege/
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
@@ -90,6 +90,11 @@ function respond() {
   else if(request.text && botRegextrades.test(request.text)) {
     this.res.writeHead(200);
     postMessage("http://bit.ly/1ZKz98I");
+    this.res.end();
+  }
+  else if(request.text && botRegexFats.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://media.giphy.com/media/11gZBGuDnYwdpu/giphy.gif");
     this.res.end();
   }
   else if(request.text && botRegexSiege.test(request.text)) {
