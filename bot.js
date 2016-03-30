0e1dae6d94e3951eeafa90a482fef37c7d631890
alextanzilli <alextanzilli@gmail.com>
2016-03-30 15:48:46 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,7 @@ function respond() {
       botRegexmfl = /^\/mflpa/;  botRegexDL = /^\/DDL/i; botRegexSalt = /^\/salt/; botRegexRules = /^\/rules/
       botRegexAd=/^\/advance/; botRegexGTA = /^\/gta/; botRegexsdl = /^\/sdl/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexporp = /^\/purpose/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegextrades = /^\/trades/; botRegexFats = /^\/404/; botRegexWAT = /^\/wat/;
+      botRegexPurp = /^\/porpoise/;
       botRegexSiege = /^\/siege/
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
@@ -100,6 +101,11 @@ function respond() {
     else if(request.text && botRegexWAT.test(request.text)) {
     this.res.writeHead(200);
     postMessage("http://i.imgur.com/AxtzziK.gif");
+    this.res.end();
+  }
+  else if(request.text && botRegexPurp.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://engaged.robbinsbrothers.com/wp-content/uploads/2014/01/propose-live-on-tv.jpeg");
     this.res.end();
   }
   else if(request.text && botRegexSiege.test(request.text)) {
