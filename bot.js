46a1d708ab245ec67df454938293a1f1b2e03134
alextanzilli <alextanzilli@gmail.com>
2016-03-31 17:44:36 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ function respond() {
       botRegexmfl = /^\/mflpa/;  botRegexDL = /^\/DDL/i; botRegexSalt = /^\/salt/; botRegexRules = /^\/rules/
       botRegexAd=/^\/advance/; botRegexGTA = /^\/gta/; botRegexsdl = /^\/sdl/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexporp = /^\/purpose/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegextrades = /^\/trades/; botRegexFats = /^\/404/; botRegexWAT = /^\/wat/;
-      botRegexPurp = /^\/porpoise/; botRegexProp = /^\/propose/; botRegexDDL = /^\/ddl/i;
+      botRegexPurp = /^\/porpoise/; botRegexProp = /^\/propose/;
       botRegexSiege = /^\/siege/
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
@@ -22,7 +22,7 @@ function respond() {
   } 
   else if(request.text && botRegexDL.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://daddyleagues.com/MFLPA");
+    postMessage("http://daddyleagues.com/MFLPA/team/"+request.text.substring(5,8)+"/depthchart");
     this.res.end();
   } 
   else if(request.text && botRegexSalt.test(request.text)) {
@@ -113,11 +113,6 @@ function respond() {
     postMessage("https://media-mediatemple.netdna-ssl.com/wp-content/uploads/2014/05/00_closed-large.png");
     this.res.end();
   } 
-  else if(request.text && botRegexDDL.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("http://daddyleagues.com/MFLPA/team/"+request.text.substring(5,8)+"/depthchart");
-    this.res.end();  
-  }
   else if(request.text && botRegexSiege.test(request.text)) {
     this.res.writeHead(200);
     if(0.6 >= Math.random() > 0.3)
