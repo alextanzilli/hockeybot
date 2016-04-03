a8417be561e5eb2a9ada391e17ba9ff134394548
alextanzilli <alextanzilli@gmail.com>
2016-04-03 13:32:54 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ function respond() {
       botRegexAd=/^\/advance/; botRegexGTA = /^\/gta/; botRegexsdl = /^\/sdl/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexporp = /^\/purpose/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegextrades = /^\/trades/; botRegexFats = /^\/404/; botRegexWAT = /^\/wat/;
       botRegexPurp = /^\/porpoise/; botRegexProp = /^\/propose/; botRegexRec = /^\/records/; botRegexMacho = /^\/machoman/;
-      botRegexDuck2 = /^\/ducking/; botRegexTony = /^\/tony/;
+      botRegexDuck2 = /^\/ducking/; botRegexTony = /^\/tony/; botRegexDiv = /^\/division/;
       botRegexSiege = /^\/siege/
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
@@ -134,6 +134,11 @@ function respond() {
     postMessage("http://kingrichiespizza.com/wp-content/uploads/2014/05/Pepperoni1.jpg");
     this.res.end();
   }
+    else if(request.text && botRegexDiv.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://yr3ghajnsielem.files.wordpress.com/2010/06/division.png");
+    this.res.end();
+  }  
   else if(request.text && botRegexSiege.test(request.text)) {
     this.res.writeHead(200);
     if(0.6 >= Math.random() > 0.3)
