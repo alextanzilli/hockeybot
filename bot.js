2ec09d32891ab9a6cd4eec078cd272523122612f
alextanzilli <alextanzilli@gmail.com>
2016-04-09 18:43:56 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ function respond() {
       botRegexPurp = /^\/porpoise/; botRegexProp = /^\/propose/; botRegexRec = /^\/records/; botRegexMacho = /^\/machoman/;
       botRegexDuck2 = /^\/ducking/; botRegexTony = /^\/tony/; botRegexDiv = /^\/division/; botRegexJpg = /^\/jpeg/; botRegexGoat = /^\/goat/;
       botRegex420 = /^\/420/; botRegexHuh = /^\/huh/; botRegexStand = /^\/standings/; botRegexCarlos = /^\/carlos/; botRegexDET = /^\/detroitpride/;
-      botRegexAss = /^\/assemble/;
+      botRegexAss = /^\/assemble/; botRegexDD = /^\/dd/;
       botRegexSiege = /^\/siege/
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
@@ -181,6 +181,11 @@ function respond() {
     postMessage("http://i.imgur.com/ZdSr4Jo.png");
     this.res.end();
   }    
+    else if(request.text && botRegexDD.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://cdn.hitfix.com/photos/4254300/Debbie-Downer.jpg");
+    this.res.end();
+  }      
   else if(request.text && botRegexSiege.test(request.text)) {
     this.res.writeHead(200);
     if(0.6 >= Math.random() > 0.3)
