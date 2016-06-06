b205e71807b81a22cce999c147884a0acdab9b65
alextanzilli <alextanzilli@gmail.com>
2016-06-06 19:17:59 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -282,7 +282,7 @@ function respond() {
     }    
       else if(request.text && botRegexRock.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://goo.gl/forms/OH14rbQfpY9SzCP32, http://goo.gl/forms/bjwRj1PLMI0MoQvG3")
+    postMessage("http://goo.gl/forms/gJbJ2c1i2qCG2N5d2")
     this.res.end();
     }      
   else if(request.text && botRegexSiege.test(request.text)) {
