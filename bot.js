5de507d6df621d64388da0774aa4571ae4c82f02
alextanzilli <alextanzilli@gmail.com>
2016-06-20 19:41:41 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegexNHL = /^\/puck/; botRegexDuck = /^\/duck/; botRegexGoat = /^\/goat/;
+      botRegexNHL = /^\/puck/; botRegexDuck = /^\/duck/; botRegexGoat = /^\/goat/; botRegexTw = /^\/twitch/i;
       botRegexSiege = /^\/siege/
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
@@ -27,6 +27,11 @@ function respond() {
     postMessage("http://image.nj.com/home/njo-media/width620/img/flyers_main/photo/16385719-mmmain.jpg")
     this.res.end();
   }
+    else if(request.text && botRegexTw.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://www.twitch.tv/"+request.text.substring(8,request.text.length));
+    this.res.end();
+  } 
   else if(request.text && botRegexSiege.test(request.text)) {
     this.res.writeHead(200);
     if(0.6 >= Math.random() > 0.3)
